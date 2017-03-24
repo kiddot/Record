97efd0c83d87262c5fa55e52f378e95e3f9ec9c8
kiddot <kiddo1024@163.com>
2017-03-24 16:19:53 +0800

    封装greendao

--- app/src/main/java/com/android/record/RecordApplication.java
+++ app/src/main/java/com/android/record/RecordApplication.java
@@ -2,6 +2,10 @@ package com.android.record;
 
 import android.app.Application;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.android.record.greendao.DaoMaster;
+import com.android.record.greendao.DaoSession;
 
 /**
  * Created by kiddo on 17-3-19.
@@ -9,14 +13,48 @@ import android.content.Context;
 
 public class RecordApplication extends Application{
     public static Context mContext;
+    public static RecordApplication mApplication;
+    private DaoMaster.DevOpenHelper mHelper;
+    private SQLiteDatabase db;
+    private DaoMaster mDaoMaster;
+    private DaoSession mDaoSession;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mApplication = this;
         mContext = getApplicationContext();
+        setDatabase();
+    }
+
+    public static RecordApplication getApplication() {
+        return mApplication;
     }
 
     public static Context getContext(){
         return mContext;
+    }
+
+    private void setDatabase() {
+
+        // 通过DaoMaster 的内部类 DevOpenHelper，你可以得到一个便利的SQLiteOpenHelper 对象。
+        // 可能你已经注意到了，你并不需要去编写「CREATE TABLE」这样的 SQL 语句，因为greenDAO 已经帮你做了。
+        // 注意：默认的DaoMaster.DevOpenHelper 会在数据库升级时，删除所有的表，意味着这将导致数据的丢失。
+        // 所以，在正式的项目中，你还应该做一层封装，来实现数据库的安全升级。
+        mHelper = new DaoMaster.DevOpenHelper(this,"notes-db", null);
+        db =mHelper.getWritableDatabase();
+        // 注意：该数据库连接属于DaoMaster，所以多个 Session 指的是相同的数据库连接。
+        mDaoMaster = new DaoMaster(db);
+        mDaoSession = mDaoMaster.newSession();
+
+    }
+
+    public DaoSession getDaoSession() {
+        return mDaoSession;
+    }
+
+
+    public SQLiteDatabase getDb() {
+        return db;
     }
 }
--- app/src/main/java/com/android/record/base/dao/DaoService.java
+++ app/src/main/java/com/android/record/base/dao/DaoService.java
@@ -1,41 +1,42 @@
-//package com.android.record.base.dao;
-//
-//import java.util.List;
-//import java.util.concurrent.Callable;
-//
-//import de.greenrobot.dao.query.QueryBuilder;
-//
-///**
-// * 基于GreenDao的数据层服务接口
-// * Created on 2017/1/9
-// */
-//public interface DaoService {
-//
-//    <T> void insert(T t);
-//
-//    <T> void update(T t);
-//
-//    <T> void delete(T t);
-//
-//    <T> void deleteAll(Class<T> t);
-//
-//    <T> List<T> loadAll(Class<T> t);
-//
-//    <T> QueryBuilder<T> query(Class<T> t);
-//
-//    /**
-//     * 在事务中,运行runnable操作
-//     *
-//     * @param runnable
-//     */
-//    void runInTx(Runnable runnable);
-//
-//    /**
-//     * 在事务中，运行callable操作
-//     *
-//     * @param callable
-//     * @param <V>
-//     * @return
-//     */
-//    <V> V callInTx(Callable<V> callable) throws Exception;
-//}
+package com.android.record.base.dao;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+
+/**
+ * 基于GreenDao的数据层服务接口
+ * Created on 2017/1/9
+ */
+public interface DaoService {
+
+    <T> void insert(T t);
+
+    <T> void update(T t);
+
+    <T> void delete(T t);
+
+    <T> void deleteAll(Class<T> t);
+
+    <T> List<T> loadAll(Class<T> t);
+
+    <T> QueryBuilder<T> query(Class<T> t);
+
+    /**
+     * 在事务中,运行runnable操作
+     *
+     * @param runnable
+     */
+    void runInTx(Runnable runnable);
+
+    /**
+     * 在事务中，运行callable操作
+     *
+     * @param callable
+     * @param <V>
+     * @return
+     */
+    <V> V callInTx(Callable<V> callable) throws Exception;
+}
