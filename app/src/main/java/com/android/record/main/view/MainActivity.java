06097892a477e62d74dda3a88a6eb839dfb5528a
kiddot <kiddo1024@163.com>
2017-03-28 20:42:07 +0800

    design main

--- app/src/main/java/com/android/record/main/view/MainActivity.java
+++ app/src/main/java/com/android/record/main/view/MainActivity.java
@@ -2,25 +2,26 @@ package com.android.record.main.view;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.android.record.R;
 import com.android.record.base.componet.BaseActivity;
+import com.github.clans.fab.FloatingActionButton;
 
-import yalantis.com.sidemenu.util.ViewAnimator;
 
 /**
  * Created by kiddo on 17-3-28.
  */
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements FloatingActionButton.OnClickListener{
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private FloatingActionButton mFabOne;
+    private FloatingActionButton mFabSecond;
+    private FloatingActionButton mFabThird;
+    private FloatingActionButton mFabFourth;
+
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context, MainActivity.class);
@@ -35,6 +36,37 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void init(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
+        initView();
+        addListener();
     }
 
+    private void addListener() {
+        mFabOne.setOnClickListener(this);
+        mFabSecond.setOnClickListener(this);
+        mFabThird.setOnClickListener(this);
+        mFabFourth.setOnClickListener(this);
+    }
+
+    private void initView() {
+        mFabOne = (FloatingActionButton) findViewById(R.id.menu_item_one);
+        mFabSecond = (FloatingActionButton) findViewById(R.id.menu_item_second);
+        mFabThird = (FloatingActionButton) findViewById(R.id.menu_item_third);
+        mFabFourth = (FloatingActionButton) findViewById(R.id.menu_item_fourth);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.menu_item_one:
+                showToast("one");
+                break;
+            case R.id.menu_item_second:
+                showToast("two");
+                break;
+            case R.id.menu_item_third:
+                break;
+            case R.id.menu_item_fourth:
+                break;
+        }
+    }
 }
