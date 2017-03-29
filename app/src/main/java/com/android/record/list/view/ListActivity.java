2fd148771fec6f883efa1d503f167bb997023c58
kiddot <kiddo1024@163.com>
2017-03-29 23:26:37 +0800

    design listActivity

--- app/src/main/java/com/android/record/list/view/ListActivity.java
+++ app/src/main/java/com/android/record/list/view/ListActivity.java
@@ -12,6 +12,7 @@ import com.android.record.base.componet.BaseActivity;
 import com.android.record.list.SwipeCardBean;
 import com.android.record.list.TanTanCallback;
 import com.android.record.list.adapter.ListAdapter;
+import com.android.record.list.fragment.InputDialog;
 import com.mcxtzhang.layoutmanager.swipecard.CardConfig;
 import com.mcxtzhang.layoutmanager.swipecard.OverLayCardLayoutManager;
 
@@ -21,12 +22,13 @@ import java.util.List;
  * Created by kiddo on 17-3-29.
  */
 
-public class ListActivity extends BaseActivity {
+public class ListActivity extends BaseActivity implements InputDialog.InputListener{
     public static final String TAG = ListActivity.class.getSimpleName();
 
     private RecyclerView mRecyclerView;
     private ListAdapter mAdapter;
     private List<SwipeCardBean> mDatas;
+    private InputDialog mInputDialog;
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context, ListActivity.class);
@@ -35,17 +37,23 @@ public class ListActivity extends BaseActivity {
 
     @Override
     protected void getLayoutBinding() {
-
+        setContentView(R.layout.activity_list);
     }
 
     @Override
     protected void init(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_list);
+        initView();
+        setConfig();
+    }
+
+    public void initView(){
         mAdapter = new ListAdapter(this,mDatas = SwipeCardBean.initDatas(),R.layout.part_item_list);
         mRecyclerView = (RecyclerView) findViewById(R.id.rv);
         mRecyclerView.setLayoutManager(new OverLayCardLayoutManager());
         mRecyclerView.setAdapter(mAdapter);
+    }
 
+    public void setConfig(){
         CardConfig.initConfig(this);
 
         final TanTanCallback callback = new TanTanCallback(mRecyclerView, mAdapter, mDatas);
@@ -55,10 +63,48 @@ public class ListActivity extends BaseActivity {
 
         final ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
         itemTouchHelper.attachToRecyclerView(mRecyclerView);
-
     }
 
     public void editCard(View view){
+        showInputDialog();
+    }
 
+    public void search(View view){
+        //TODO
+    }
+
+    public void addCard(View view){
+        showToast("click");
+        mDatas.add(new SwipeCardBean(9,null,"ldk"));
+        mAdapter.addDatas(mDatas);
+        //TODO
+    }
+
+    /**
+     * 弹出输入框
+     */
+    private void showInputDialog() {
+        mInputDialog = new InputDialog();
+        mInputDialog.show(getSupportFragmentManager(), "inputDialog");
+    }
+
+    /**
+     * 关闭输入框
+     */
+    private void dismissInputDialog() {
+        if (mInputDialog != null) {
+            mInputDialog.dismiss();
+        }
+    }
+
+    /**
+     * 获取输入框的内容
+     *
+     * @param text
+     */
+    @Override
+    public void onInputComplete(String text) {
+        //TODO
+        dismissInputDialog();
     }
 }
