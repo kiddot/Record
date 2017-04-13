57b04559a82f0ba456819f9ccea40f6318672cab
kiddot <kiddo1024@163.com>
2017-04-13 18:15:23 +0800

    create diary layout

--- app/src/main/java/com/android/record/diary/view/DiaryActivity.java
+++ app/src/main/java/com/android/record/diary/view/DiaryActivity.java
@@ -1,26 +1,38 @@
 package com.android.record.diary.view;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.android.record.R;
 import com.android.record.base.componet.BaseActivity;
+import com.android.record.base.util.ActivityUtils;
+import com.android.record.diary.presenter.DiaryPresenter;
 
 /**
  * Created by kiddo on 17-4-13.
  */
 
 public class DiaryActivity extends BaseActivity{
+    public static final String TAG = DiaryActivity.class.getSimpleName();
+    private DiaryFragment mFragment;
+    private DiaryPresenter mDiaryPrsenter;
 
-    public static void startActivity(){
-
+    public static void startActivity(Context context){
+        Intent intent = new Intent(context, DiaryActivity.class);
+        context.startActivity(intent);
     }
 
     @Override
     protected void getLayoutBinding() {
-
+        setContentView(R.layout.activity_diary);
     }
 
     @Override
     protected void init(Bundle savedInstanceState) {
-
+        mFragment = new DiaryFragment();
+        ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), mFragment, R.id.diary_fl_container);
+        mDiaryPrsenter = new DiaryPresenter(mFragment);
+        mFragment.setPresenter(mDiaryPrsenter);
     }
 }
--- app/src/main/java/com/android/record/diary/view/DiaryFragment.java
+++ app/src/main/java/com/android/record/diary/view/DiaryFragment.java
@@ -1,8 +1,45 @@
 package com.android.record.diary.view;
 
+import com.android.record.base.componet.BaseFragment;
+import com.android.record.diary.contract.DiaryTaskContract;
+
 /**
  * Created by kiddo on 17-4-13.
  */
 
-public class DiaryFragment {
+public class DiaryFragment extends BaseFragment implements DiaryTaskContract.View{
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    public String getContent() {
+        return null;
+    }
+
+    @Override
+    public String getUserName() {
+        return null;
+    }
+
+    @Override
+    public String getDiaryTime() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public void setPresenter(DiaryTaskContract.Presenter presenter) {
+
+    }
 }
