20c9b31d4b394818a2460e193f83f515a9da0ba7
kiddot <kiddo1024@163.com>
2017-04-16 21:34:59 +0800

    create diary entry

--- app/src/main/java/com/android/record/diary/adapter/EntryAdapter.java
+++ app/src/main/java/com/android/record/diary/adapter/EntryAdapter.java
@@ -1,11 +1,17 @@
 package com.android.record.diary.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 
+import com.android.record.R;
+import com.android.record.base.componet.BaseRvAdapter;
+import com.android.record.base.componet.BaseVH;
 import com.android.record.diary.bean.EntryBean;
 
 import java.util.List;
@@ -14,30 +20,39 @@ import java.util.List;
  * Created by kiddo on 17-4-14.
  */
 
-public class EntryAdapter extends BaseAdapter {
+public class EntryAdapter extends BaseRvAdapter {
+    public static final String TAG = EntryAdapter.class.getSimpleName();
+    public static final int NORMAL_ENTRY = 0;
     private List<EntryBean> mData;
 
-    public EntryAdapter(List<EntryBean> data) {
-        this.mData = data;
+    public EntryAdapter(Context context, List list) {
+        super(context, list);
+        mData = list;
     }
 
     @Override
-    public int getCount() {
-        return mData.size();
+    public int getCustomViewType(int position) {
+        return NORMAL_ENTRY;
     }
 
     @Override
-    public Object getItem(int position) {
-        return mData.get(position);
+    public BaseVH createCustomViewHolder(ViewGroup parent, int viewType) {
+        return new EntryHolder(R.layout.item_topic_diary, parent);
     }
 
     @Override
-    public long getItemId(int position) {
-        return position;
+    public void bindCustomViewHolder(BaseVH holder, int position) {
+        EntryHolder entryHolder = (EntryHolder) holder;
+        ((EntryHolder) holder).entryType.setText(mData.get(position).getName());
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public class EntryHolder extends BaseVH{
+        TextView entryType;
+
+        public EntryHolder(int resId, ViewGroup parent) {
+            super(resId, parent);
+        }
     }
+
+
 }
--- app/src/main/java/com/android/record/diary/view/EntryFragment.java
+++ app/src/main/java/com/android/record/diary/view/EntryFragment.java
@@ -1,13 +1,23 @@
 package com.android.record.diary.view;
 
+import android.support.v7.widget.RecyclerView;
+
 import com.android.record.R;
 import com.android.record.base.componet.BaseFragment;
+import com.android.record.diary.adapter.EntryAdapter;
+import com.android.record.diary.bean.EntryBean;
+
+import java.util.List;
 
 /**
  * Created by kiddo on 17-4-14.
  */
 
 public class EntryFragment extends BaseFragment{
+    public static final String TAG = EntryFragment.class.getSimpleName();
+    private List<EntryBean> mEntryList;
+    private RecyclerView mRecyclerView;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_entry;
@@ -15,6 +25,19 @@ public class EntryFragment extends BaseFragment{
 
     @Override
     protected void init() {
+        initView();
+        initEntryData();
+        EntryAdapter adapter = new EntryAdapter(getActivity(), mEntryList);
+        mRecyclerView.setAdapter(adapter);
+    }
 
+    private void initView() {
+        mRecyclerView = (RecyclerView) getActivity().findViewById(R.id.RecyclerView_topic);
+    }
+
+    private void initEntryData(){
+        EntryBean entryBean = new EntryBean();
+        entryBean.setName("日记");
+        mEntryList.add(entryBean);
     }
 }
